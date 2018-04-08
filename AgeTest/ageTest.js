e21f74d23072b2afb1f60bc40d3830d95ce90460
Kenneth Barnes <kennethbarnes@Kenneths-MacBook-Air.local>
2018-04-08 10:52:18 -0400

    Added new activity from practice

---
+++ AgeTest/ageTest.js
@@ -0,0 +1,23 @@
+//var age = prompt("What is your age?");
+//if(age <= 18){
+    //alert("Sorry, you are too young to get into this club!")
+//}
+//else if(age > 18 && age < 21){
+  //  alert("Welcome to the club, BUT you cannot have a drink homie!")
+//}
+//else{
+  //  alert("Welcome to the club! The first cheap drink is on us!")
+//}
+
+var age = prompt("What is your age?");
+if(age < 0){
+    alert("Error")
+}
+
+else if(age === 21){
+    alert("Happy Birthday!")
+}
+//If age is odd
+if(age % 2 !== 0){
+    alert("Your age is ODD!")
+}
